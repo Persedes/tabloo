1e9bfb3c53078df2a8b52aa1e150bf8d3a61ce16
mheiser <mheiser@metabolon.com>
2022-06-14 14:05:40 -0400

    Added option to load xlsx files too

--- src_backend_python/tabloo/tabloo_cli.py
+++ src_backend_python/tabloo/tabloo_cli.py
@@ -7,6 +7,7 @@ from __future__ import division, print_function
 
 import argparse
 import sys
+import os
 
 import pandas as pd
 
@@ -18,7 +19,7 @@ def parse_args(args=sys.argv[1:]):
     parser.add_argument(
         "file",
         metavar="<FILE>",
-        help="CSV file to load.",
+        help="CSV/xlsx file to load.",
     )
     parser.add_argument(
         "--sep",
@@ -28,13 +29,45 @@ def parse_args(args=sys.argv[1:]):
     return args
 
 
-def main():
-    args = parse_args()
-
+def load_csv(args):
     loader_kwargs = dict()
     if args.sep is not None:
         loader_kwargs["sep"] = args.sep
     df = pd.read_csv(args.file, **loader_kwargs)
+    return df
+
+
+def load_excel(args):
+    df = pd.read_excel(args.file)
+    return df
+
+
+FILE_LOADERS = {
+    "csv": load_csv,
+    "xlsx": load_excel,
+}
+
+
+def load_file(args):
+    *_, extension = os.path.splitext(args.file)
+    if not extension:
+        raise ValueError(f"No File extension found in {args.file}")
+
+    extension = extension.lower().strip(".")
+
+    try:
+        df = FILE_LOADERS[extension](args)
+        return df
+    except (KeyError):
+        raise NotImplementedError("File extension {extension} currently not supported")
+
+
+
+def main():
+    args = parse_args()
+
+    df = load_file(args)
+    print(df)
 
     tabloo.show(
         df,
